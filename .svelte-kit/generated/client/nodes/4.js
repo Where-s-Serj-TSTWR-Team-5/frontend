fadc06505c140ae10ca9e1ff5ebc7f15c3e3268b
Miguel Brugge <brug0024@hz.nl>
2025-12-12 21:19:21 +0100

    Remove .svelte-kit from repo

--- .svelte-kit/generated/client/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/events/[id]/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/events/[id]/+page.svelte";
